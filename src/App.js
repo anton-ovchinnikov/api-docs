2c608b9cfd08e02ef666ec18dff754f054cdad91
Anton Ovchinnikov <anton.ovchinnikov.737@gmail.com>
2023-04-28 00:46:28 +0700

    Роуты + заглушки страниц

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Navbar from "./components/UI/Navbar/Navbar";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Footer from "./components/UI/Footer/Footer";
+import HomePage from "./pages/HomePage/HomePage";
+import ContactsPage from "./pages/ContactsPage/ContactsPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
     return (
         <BrowserRouter>
             <Navbar/>
+            <Routes>
+                <Route element={<HomePage/>} path='/'/>
+                <Route element={<ContactsPage/>} path='/contacts'/>
+                <Route element={<NotFoundPage/>} path='*'/>
+            </Routes>
             <Footer/>
         </BrowserRouter>
     );
