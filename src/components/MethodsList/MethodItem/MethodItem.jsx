6215165f9eaa2988b6200508b15d1b92300f1ae0
Anton Ovchinnikov <anton.ovchinnikov.737@gmail.com>
2023-04-29 19:24:17 +0700

    Отображение методов + компонент для метода

--- src/components/MethodsList/MethodItem/MethodItem.jsx
+++ src/components/MethodsList/MethodItem/MethodItem.jsx
@@ -1,36 +1,38 @@
 import React from 'react';
 import classes from "./MethodItem.module.css";
 
-const MethodItem = () => {
+const MethodItem = ({method, id}) => {
     return (
         <div className="accordion mb-3">
             <div className="accordion-item">
                 <h2 className={`${classes.header} accordion-header`}>
                     <div className="accordion-button collapsed" itemType="button"
-                         data-bs-toggle="collapse" data-bs-target="#flush-Авторизация_мерча" aria-expanded="false"
-                         aria-controls="flush-Авторизация_мерча">
-                        <span className="btn btn-success" itemType="alert">POST</span>
+                         data-bs-toggle="collapse" data-bs-target={`#flush-${id}`}
+                         aria-expanded="false"
+                         aria-controls={`flush-${id}`}>
+                        {method['type'] === 'POST' ? <span className="btn btn-success" itemType="alert">POST</span> :
+                            <span className="btn btn-success" itemType="alert">POST</span>}
                         &emsp;
                         <span className={classes.header_info}>
-                            <b>https://merch-api.yooyo.ru/auth</b>
+                            <b>{method['route']}</b>
                             &emsp;
-                            <span className={classes.title}>Авторизация мерча</span>
+                            <span className={classes.title}>{method['name']}</span>
                         </span>
                     </div>
                 </h2>
-                <div id="flush-Авторизация_мерча" className="accordion-collapse collapse">
+                <div id={`flush-${id}`} className="accordion-collapse collapse">
                     <div className="accordion-body p-0 m-0">
                         <div className={`${classes.description} row p-4 py-3 m-0`}>
-                            Авториазция мерча. В ответе получаем куки с токенами и сохраняем их
+                            {method['description']}
                         </div>
-                        <div className={`${classes.description} row p-4 py-3 m-0`}>Передать device-id</div>
+                        <div className={`${classes.description} row p-4 py-3 m-0`}>{method['parameters']}</div>
                         <div className="row p-4 py-3 m-0">Body</div>
                         <div className={`${classes.code} row p-5 py-4 m-0`}>
-                            bodyyy
+                            {method['body']}
                         </div>
                         <div className="row p-4 py-3 m-0">Response</div>
                         <div className={`${classes.code} row p-5 py-4 m-0`}>
-                            resp
+                            {method['response']}
                         </div>
                     </div>
                 </div>
--- src/components/MethodsList/MethodsList.jsx
+++ src/components/MethodsList/MethodsList.jsx
@@ -2,9 +2,33 @@ import React from 'react';
 import MethodItem from "./MethodItem/MethodItem";
 
 const MethodsList = () => {
+    const methods = {
+        "-NU7n4bPEPmzPjczsDrC": {
+            "body": "{\"email\": \"1\",\"password\": \"2\",}",
+            "description": "description",
+            "name": "name",
+            "parameters": "parameters",
+            "response": "{\"code\": 200,\"message\": \"Успех!\",}",
+            "route": "https://route.ru",
+            "section": "section",
+            "type": "POST"
+        },
+        "-NU7nLEI_hzRET9V4pK6": {
+            "body": "{\"login\": \"1\",\"password\": \"2\",}",
+            "description": "description",
+            "name": "name",
+            "parameters": "parameters",
+            "response": "{\"code\": 200,\"message\": \"Успех!\",}",
+            "route": "https://route.com",
+            "section": "section",
+            "type": "GET"
+        }
+    }
+    let method_ids = Object.keys(methods);
+
     return (
         <div className="container my-4">
-            <MethodItem/>
+            {method_ids.map(key => <MethodItem key={key} method={methods[key]} id={key}/>)}
         </div>
     );
 };
